4318adf022a1103658c40567f4333b947e5e680c
Carlos Sánchez <oceanrdn@gmail.com>
2024-01-16 19:26:28 +0100

    Contract cleanup

---
+++ packages/hardhat/contracts/GhoFundStreams.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "hardhat/console.sol";
+
+// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GhoFundStreams {
+	/**
+	 * Function that allows the contract to receive ETH
+	 */
+	receive() external payable {}
+}
