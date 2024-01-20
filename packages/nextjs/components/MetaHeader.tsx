202f15b50b6b1046e123fdc2fb00eb137fb1322e
Carlos Sánchez <oceanrdn@gmail.com>
2024-01-20 10:16:30 +0100

    Metas

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -14,8 +14,8 @@ type MetaHeaderProps = {
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
-  title = "Scaffold-ETH 2 App",
-  description = "Built with 🏗 Scaffold-ETH 2",
+  title = "GHOFund",
+  description = "Forkable Treasury Management Platform. Built with 🏗 Scaffold-ETH 2 for the 👻 LFGHO Hackathon.",
   image = "thumbnail.jpg",
   twitterCard = "summary_large_image",
   children,
